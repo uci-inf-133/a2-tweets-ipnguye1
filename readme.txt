9a994913689df4709860a1c6d7e7e7bb0e297b35
Isabella Nguyen <ipnguye1@uci.edu>
2025-10-29 02:39:04 -0700

    still working on description.js but committing here in case i fuck up lol

--- readme.txt
+++ readme.txt
@@ -2,9 +2,9 @@
 
 1. How many assignment points do you believe you completed (replace the *'s with your numbers)?
 
-*/10
+10/10
 - 3/3 Summarizing tweets
-- */4 Identifying the most popular activities
+- 4/4 Identifying the most popular activities
 - 3/3 Adding a text earch interface
 
 2. How long, in hours, did it take you to complete this assignment?
@@ -15,6 +15,7 @@ october 24th nigh: 30 minutes
 octiber 25th night: 6 hours
 octiber 26th night: 2 hours
 october 27th night: like 1.5 hours idk
+october 28th night: 4 hours?
 
 3. What online resources did you consult when completing this assignment? (list sites like StackOverflow or specific URLs for tutorials; describe queries to Generative AI or use of AI-based code completion)
 https://www.w3schools.com/jsref/jsref_join.asp
@@ -35,6 +36,7 @@ https://developer.mozilla.org/en-US/docs/Web/API/Element/keyup_event
 https://www.w3schools.com/jsref/met_document_createelement.asp
 https://www.w3schools.com/js/js_promise.asp
 https://www.w3schools.com/js/js_comparisons.asp
+https://www.w3schools.com/jsref/jsref_push.asp
 
 https://www.w3schools.com/js/js_switch.asp
 https://vega.github.io/vega-lite/examples/window_top_k.html
@@ -47,8 +49,8 @@ https://vega.github.io/vega-lite/docs/aggregate.html
 
 
 4. What classmates or other individuals did you consult as part of this assignment? What did you discuss?
-I did not consult with anyone.
+I did not consult anyone.
 
 
 5. Is there anything special we need to know in order to run your code?
-For descriptions.js, I edited the default DOMContentLoaded listener code because my search code cannot work otherwise.
+For descriptions.js, I edited the default DOMContentLoaded listener code to run loadSavedRunkeeperTweets().then(parseTweets) before addEventHandlerForSearch() because my search code cannot work otherwise.
--- ts/tweet.ts
+++ ts/tweet.ts
@@ -18,25 +18,32 @@ class Tweet {
         } else {
             return "miscellaneous";
         }
-        //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
     }
 
-    //returns a boolean, whether the text includes any content written by the person tweeting.
-    // ?????????????????? ok idfk if this really is correct lmfao
+    //returns whether the text includes any content written by the person tweeting
     get written():boolean {
         return this.text.includes("-");
     }
 
+    // returns the tweet's written text
     get writtenText():string {
         if(!this.written) {
             return "";
         }
-        var beginningOfText = this.text.indexOf("-") + 1;
+        // written text begins right after the -
+        var beginningOfWrittenText = this.text.indexOf("-") + 1; 
 
-        //TODO: parse the written text from the tweet
-        return this.text.substring(beginningOfText);
+        // isolate written text
+        var writtenText = this.text.substring(beginningOfWrittenText);
+
+        // find last link in the tweet which should be the tweet link
+        // (since it's possible for users to put in links of their own for custom text)
+        var endOfWrittenText = writtenText.lastIndexOf("https://t.co/");
+
+        return writtenText.substring(0, endOfWrittenText);
     }
 
+    // returns what the user did
     get activityType():string {
         if (this.source != 'completed_event') {
             return "unknown";
@@ -118,49 +125,40 @@ class Tweet {
             return 0;
         }
 
+        // split tweet along the spaces
         var tweetTextArray = this.text.split(" ");
-        if (tweetTextArray[4] === "mi" || tweetTextArray[4] === "km") {
+
+        // Completed events always begin with "Just posted a (insert number) (mi/km)" 
+        // so we can assume that the 4th word is the distance unit and the 3rd word is distance
+        if (tweetTextArray[4] === "mi" || tweetTextArray[4] === "km") { 
             return Number.parseFloat(tweetTextArray[3]);
         } else { // activities measured in time do not have mi or km
             return 0;
         }
-
-        // console.log(tweetTextArray);
-
-        //format this later???
-        // if (tweetTextArray.includes("mi") && 
-        // (tweetTextArray.indexOf("mi") < tweetTextArray.indexOf("-"))) {
-        //     var distance = tweetTextArray[tweetTextArray.indexOf("mi") - 1];
-        //     return Number.parseFloat(distance);
-        // } else if (tweetTextArray.includes("km") && 
-        // (tweetTextArray.indexOf("km") < tweetTextArray.indexOf("-"))) {
-        //     var distance = tweetTextArray[tweetTextArray.indexOf("km") - 1];
-        //     return Number.parseFloat(distance);
-        // } else { 
-        //     // activities measured in time do not have mi or km, or at least mi/km BEFORE custom text indicator
-        //     return 0;
-        // }
     }
 
     getHTMLTableRow(rowNumber:number):string {
-        //TODO: return a table row which summarizes the tweet with a clickable link to the RunKeeper activity
+        // split tweet along the spaces
         var tweetTextArray = this.text.split(" ");
 
+        // look for link location
         var linkIndex = -1;
-        for (var i = 0; i < tweetTextArray.length; i++) { // look for link location
+        for (var i = 0; i < tweetTextArray.length; i++) {
+            // adds the link into the tweet
             if (tweetTextArray[i].includes("https://t.co/")) {
                 linkIndex = i;
-                break;
+                var link = "<a href=\"" + tweetTextArray[linkIndex] + "\">"; // add beginning <a> tag
+                link += tweetTextArray[linkIndex]; // add URL for clickable text
+                link += "</a>"; //add </a> tag
+                tweetTextArray[i] = link; // insert clickable link into link location
             }
         }
 
-        var link = "<a href=\"" + tweetTextArray[linkIndex] + "\">"; // add beginning <a> tag
-        link += tweetTextArray[linkIndex]; // add URL for clickable text
-        link += "</a>"; //add </a> tag
-        tweetTextArray[i] = link; // insert clickable link into link location
-
+        // put the tweet back together
         var tweetText = tweetTextArray.join(" ");
-        var returnStr = "<td>" + rowNumber + "</td><td>" + this.activityType + "</td><td>" + tweetText + "</td>";
+
+        // tweet text with the clickable link
+        var returnStr = "<td>" + (rowNumber + 1) + "</td><td>" + this.activityType + "</td><td>" + tweetText + "</td>";
         return returnStr;
     }
 }
